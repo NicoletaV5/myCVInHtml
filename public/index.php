3393697913d1dc6ca815e03494e60d1b192e57e6
NicoletaV5 <79136023+NicoletaV5@users.noreply.github.com>
2021-06-10 23:06:14 +0300

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV - Nicoleta VOLOSCIUC</title>
+</head>
+<body>
+    <header>
+        <h1>Portofolio</h1>
+        <nav>
+            <ul>
+                <li> <a href="#about"> About </a></li>
+                <li> <a href="#education">Education</a> </li>
+                <li> <a href="#skills"> Skills</a> </li>
+                <li> <a href="#experience">Experience</a> </li>
+                <li> <a href="#contact">Contact</a> </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <section id="about">
+            <h2>About</h2>
+            <table>
+                <tr>
+                    <td>
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1gVjdRyKvH0UPujF47ofxuSwlckeoPkrmyXHWQCXUrFtyHJL5EotPozfFb8HtrMrQOUg&usqp=CAU" alt="" width="150" height="150">
+                    </td>
+
+                    <td>
+                        <ul>
+                            <li>Nicoleta Volosciuc</li>
+                            <li>Date of birth: 05.02.2002</li>
+                            <li>Gender: Female</li>
+                            <li>Email: volosciuc.nicoleta74@gmail.com</li>
+                        </ul>
+                    </td>
+                </tr>
+            </table>
+             <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum itaque fugit delectus obcaecati deserunt voluptate ad reprehenderit eligendi sapiente! Inventore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quam tempora facere architecto deserunt aliquam in eaque totam. Dicta, vel? Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, explicabo.
+            </p>
+        </section>
+
+        <section id="education">
+            <h2>Education</h2>
+            <details>
+                <summary> CEITI </summary>
+                <p> 
+                    Programming and analysis programs, 2017-2021 (Chisinau, Moldova)
+                </p>
+            </details>
+            <details>
+                <summary> Gymnasium "V. Slobozenco" </summary>
+                <p> 
+                    General studies, 2008-2017 (Micauti, Straseni)
+                </p>
+            </details>
+        </section>
+
+        <section id="skills">
+            <h2>Skills</h2>
+            <ul type="square">
+                <li>Digital skills
+                    <ul>
+                    <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JS</li>
+                        <li>PHP</li>
+                        <li>Laravel</li>
+                        <li>Bootstrap</li>
+                    </ul>
+                </li> <br>
+                <li> Language Skills
+                    <ul>
+                        <li>Romanian</li>
+                        <li>English</li>
+                        <li>Russian</li>
+                    </ul>
+                </li> <br>
+                <li> Communicational skills
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                        <li>Lorem ipsum dolor sit amet consectetur.</li>
+                        <li>Lorem ipsum dolor sit.</li>
+                        <li>Lorem ipsum dolor sit amet.</li>
+                    </ul>
+                </li>
+            </ul>
+        </section>
+        
+        <section id="experience">
+            <h2>Experience</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Period</th>
+                        <th>Place</th>
+                        <th>Position/Description</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>2018-present</td>
+                        <td>DevelopmentAid</td>
+                        <td>
+                            <section>
+                                <h3>Data Analyst</h3>
+                                <p>
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis accusamus nesciunt commodi sit necessitatibus suscipit.
+                                </p>
+                            </section>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>2015-2018</td>
+                        <td>Boekestijn</td>
+                        <td>
+                            <section>
+                                <h3>Cash Cost Specialist</h3>
+                                <p>
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis accusamus nesciunt commodi sit necessitatibus suscipit.
+                                </p>
+                            </section>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+
+        <section id="contact">
+            <h2>Contact</h2>
+            <ul>
+                <li>Tel: <a href="tel:+060217861"> +373 60217861 </a> </li>
+                <li>Email: volosciuc.nicoleta74@gmail.com</li>
+                <li>Address: Micauti, Straseni</li>
+                <li>Facebook link:</li>
+            </ul>
+        </section>
+    </main>
+    <footer>
+        <div align="center"> 
+            Copywrite &copy; 2021, DevelopmentAid
+        </div>
+    </footer>
+</body>
+</html>
